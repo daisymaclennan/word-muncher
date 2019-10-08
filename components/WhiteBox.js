9aadf6229d8f7edcc2b64e13e356918c9b058da8
Daisy Maclennan <daisy@sidigital.co>
2019-10-08 16:46:28 +0100

    Started building first page layout

---
+++ components/WhiteBox.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const WhiteBox = styled.div`
+  background: #FFFFFF;
+  box-shadow: 2px 2px 4px rgba(78, 111, 125, 0.25);
+  border-radius: 10px;
+`
+
+
+export default WhiteBox
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import Link from 'next/link'
+import api from '../lib/api'
 import Layout from '../components/layout'
+import { useCookies } from 'react-cookie'
+import { Formik, Form, Field, FieldArray } from 'formik'
+import WhiteBox from '../components/WhiteBox'
 
-export default () => (
+const Page = () => {
+  /*Gets the value of the user cookie and assigns it to the variable user*/
+  const [ cookies ] = useCookies([ 'user' ]);
+  const { user } = cookies
+
+  /*If the user cookie does not exist it will return the login form*/
+  if(!user && process.browser){
+    return(
+      <Layout>
+        <LoginForm/>
+      </Layout>
+    )
+  }
+
+  return(
   <Layout>
-    <p>Sed risus adipiscing hendrerit in vestibulum sed aenean, sodales vivamus est risus aenean vestibulum. Lorem ut sollicitudin sapien in vestibulum ut suscipit amet porttitor. Vestibulum ipsum in sapien est consectetur ante. Nunc condimentum vivamus aenean ante risus condimentum euismod, ipsum, faucibus ante in adipiscing lorem.</p>
-    <Link href='/directory'>
-      <a>Directory</a>
-    </Link>
+    <Homepage/>
   </Layout>
+)}
+
+const LoginForm = () => (
+  <>
+    <WhiteBox css={`
+      width: 350px;
+      height: 380px;
+
+      position: fixed;
+      top: 90px;
+      left: calc(50vw - 175px);
+      `}>
+      <h2>Register</h2>
+    </WhiteBox>
+  </>
 )
+
+const Homepage = () => (
+  <div>
+  </div>
+)
+
+export default Page

fbf1cf311361fb04dbbff8741aca6a69448e1f6f
Daisy Maclennan <daisy@sidigital.co>
2019-10-08 16:46:04 +0100

    API built for users

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -7,8 +7,42 @@ export default async (req, res) => {
     return;
   }
 
-  /*Selects the id and password where the username field is equal to the username submitted in the login form.
+  /*Selects the id and password where the username field is equal to the username
+  submitted in the login form.*/
   const results = await query(sql`
-    SELECT id, password FROM user WHERE username = ${req.body.username}
+    SELECT
+      id,
+      password
+    FROM
+      user
+    WHERE
+      username = ${req.body.username}
   `)
+
+  if(results.error){
+    throw results.error
+  }
+
+  /*Checks if any results have been returned*/
+  if(results.length == 0){
+    res.status(401).json({})
+    return
+  }
+
+  /*Checks if the password matches the one stored in the database*/
+  const password = await bcrypt.compare(req.body.password, results[0].password)
+
+  if(password){
+    var jwt = require('jsonwebtoken')
+    var token = jwt.sign(results[0].id, process.env.JWT_SECRET)
+
+    /*Sets the user cookie to equal the value of their access token*/
+    res.json({
+      token
+    })
+  }else{
+    console.log("Wrong password")
+    res.status(401).json({})
+  }
+
 }
